acb82a483de591a3fb866d9bcf926d178bfa744b
Star-gazzer <111698212+Star-gazzer@users.noreply.github.com>
2026-02-12 19:03:09 +0530

    Add Test_AI_Agent

---
+++ Test_AI_Agent/Test_1.py
@@ -0,0 +1,33 @@
+import os
+from typing import TypedDict, List, Dict
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+client = Groq()
+
+
+class agentstate(TypedDict):
+    messaage: list[HumanMessage]
+
+llm = ChatGroq(
+    model="llama-3.1-8b-instant")
+
+def process(state: agentstate) -> agentstate:
+    response = llm.invoke(state['messaage'])
+    print(f'\nAI: {response.content}')
+    return state
+
+graph = StateGraph(agentstate)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+app = graph.compile()
+
+user_input = input("Enter your message: ")
+while user_input != "exit":
+    app.invoke({"messaage": [HumanMessage(content=user_input)]})
+    user_input = input("Enter your message: ")
